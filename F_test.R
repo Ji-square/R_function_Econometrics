6f3b3f1086439f0a0b90934e1338dbb1a4992fab
Eduard Romero Mercedes <romero.mercedes.eduard@gmail.com>
2025-11-22 07:53:21 +0000

    Update and rename R to F_test.R

---
+++ F_test.R
@@ -0,0 +1,24 @@
+
+compare_models <- function(df, dep, indep_res, indep_unres)
+{
+  linear_reg() |> 
+    fit_xy(
+      y = df |> select(all_of(dep)),
+      x = df |> select(all_of(indep_unres))
+    ) |> 
+      glance() |> 
+      mutate(model = "unrestricted")->mod_unres
+  
+  #modelo restringido
+  linear_reg() |> 
+    fit_xy(
+      y = df |> select(all_of(dep)),
+      x = df |> select(all_of(indep_res))
+    ) |> 
+      glance() |> 
+      mutate(model = "restricted")-> mod_res
+  bind_rows(mod_unres,mod_res) |> 
+    select(model,everything())
+
+  
+}
